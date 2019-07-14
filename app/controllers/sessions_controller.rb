d82c9fc9826eda00362a411540121da3e367d78c
yuta-nabuchi <htyms6610@gmail.com>
2019-07-14 02:15:38 +0000

    10.4.1まで終了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_to user
-     else
+        redirect_back_or user 
+    else
         # エラーメッセージを作成する
         flash.now[:danger] = 'Invalid email/password combination'
         render 'new'
